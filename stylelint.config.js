f47dd0d1de2e57a1d9718f84c080c30ae0d120af
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-04-08 21:42:53 +0700

    add: Install Stylelint.

---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order'],
+  ignoreFiles: ['**/node_modules/**'],
+  rules: {
+    'block-no-empty': true,
+    'property-no-vendor-prefix': null,
+    'comment-empty-line-before': null,
+    'media-feature-range-notation': 'prefix',
+    'selector-class-pattern': '^[a-z][a-zA-Z0-9]+$',
+  },
+};

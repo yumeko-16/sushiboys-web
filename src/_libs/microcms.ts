a9a64fd29213afcb39bd358c3267cb767c66f02d
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-04-15 22:34:15 +0700

    change: We'll integrate microCMS and display the news list on the home.

--- src/_libs/microcms.ts
+++ src/_libs/microcms.ts
@@ -1,13 +1,38 @@
+import { createClient } from 'microcms-js-sdk';
+import type {
+  MicroCMSQueries,
+  MicroCMSImage,
+  MicroCMSListContent,
+} from 'microcms-js-sdk';
+
 export type Category = {
   name: string;
-};
+} & MicroCMSListContent;
 
 export type News = {
-  id: string;
   title: string;
-  category: {
-    name: string;
-  };
-  publishedAt: string;
-  createdAt: string;
+  content: string;
+  eyecatch?: MicroCMSImage;
+  category: Category;
+} & MicroCMSListContent;
+
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error('microcms_service_domain is required');
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error('Microcms_api_key is required');
+}
+
+const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+
+export const getNewsList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<News>({
+    endpoint: 'news',
+    queries,
+  });
+  return listData;
 };

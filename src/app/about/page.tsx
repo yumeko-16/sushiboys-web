3adb57a2a7b34932f6a19730bd8ed3ef79214780
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-04-19 05:28:38 +0700

    add: Add a component for a two-column layout.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,10 @@
 import { Hero } from '@/_components/Hero/Hero';
+import { Container } from '@/_components/Container/Container';
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar,
+} from '@/_components/TwoColumn/TwoColumn';
 import { PostBody } from '@/_components/PostBody/PostBody';
 import { Contact } from '@/_components/Contact/Contact';
 
@@ -7,22 +13,32 @@ export default function About() {
     <>
       <Hero />
 
-      <PostBody>
-        <p>埼玉県越生町に突如現れた国民の最後の希望。</p>
-        <p>映画マトリックスの世界観に衝撃を受け、2016年にグループ結成。</p>
-        <p>
-          自身たちが作成した楽曲は国民に真実を気付かせてしまうため、再生回数が伸びないよう政府によって厳重に管理されている。
-        </p>
-        <p>
-          メンバーはFARMHOUSE、サンテナ、DJ兼カメラマン兼マネージャー兼運転手兼スーパーバイザーのneo
-          yosikawaで構成される。
-        </p>
-        <p>
-          アヒルの形をしたゴムボートのようなものを客席に投げるLIVEに定評がある。
-        </p>
-      </PostBody>
+      <Container>
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <p>埼玉県越生町に突如現れた国民の最後の希望。</p>
+              <p>
+                映画マトリックスの世界観に衝撃を受け、2016年にグループ結成。
+              </p>
+              <p>
+                自身たちが作成した楽曲は国民に真実を気付かせてしまうため、再生回数が伸びないよう政府によって厳重に管理されている。
+              </p>
+              <p>
+                メンバーはFARMHOUSE、サンテナ、DJ兼カメラマン兼マネージャー兼運転手兼スーパーバイザーのneo
+                yosikawaで構成される。
+              </p>
+              <p>
+                アヒルの形をしたゴムボートのようなものを客席に投げるLIVEに定評がある。
+              </p>
+            </PostBody>
+          </TwoColumnMain>
 
-      <Contact />
+          <TwoColumnSidebar>
+            <Contact />
+          </TwoColumnSidebar>
+        </TwoColumn>
+      </Container>
     </>
   );
 }

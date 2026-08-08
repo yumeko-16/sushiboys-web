352ca916b86bfbb4dea20d0dcd1961c59fb79d88
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-08-08 21:14:29 +0700

    update: Display the category type on the category page.

--- src/app/news/category/[id]/page.tsx
+++ src/app/news/category/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { getCategoryDetail, getNewsList } from '@/_libs/microcms';
 import { notFound } from 'next/navigation';
 import { NewsList } from '@/_components/NewsList/NewsList';
+import { Category } from '@/_components/Category/Category';
 
 type Props = {
   params: Promise<{
@@ -10,12 +11,18 @@ type Props = {
 
 export default async function Page({ params }: Props) {
   const { id } = await params;
-
-  await getCategoryDetail(id).catch(notFound);
-
+  const category = await getCategoryDetail(id).catch(notFound);
   const { contents: news } = await getNewsList({
     filters: `category[equals]${id}`,
   });
 
-  return <NewsList news={news} />;
+  return (
+    <>
+      <p>
+        <Category category={category} /> の一覧
+      </p>
+
+      <NewsList news={news} />
+    </>
+  );
 }

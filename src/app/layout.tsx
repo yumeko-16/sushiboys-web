f42d2a7a894959ed8727d24a7344a04a9ecfef5a
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-08-17 15:17:25 +0700

    change: Change global.css to be placed under app/.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import '@/styles/globals.css';
+import './globals.css';
 import Header from '@/_components/Header';
 import Footer from '@/_components/Footer';
 
